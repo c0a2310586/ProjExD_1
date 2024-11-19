73a03ae269ea385d930503dcd6c9118d360d6832
c0a23105 <c0a2310586@edu.teu.ac.jp>
2024-11-19 17:51:17 +0900

    演習１：キーによって移動

--- pygame_image.py
+++ pygame_image.py
@@ -11,24 +11,24 @@ def main():
     clock  = pg.time.Clock()
     bg_img = pg.image.load("fig/pg_bg.jpg")  # 背景画像Surfaceを作成する
     bg_img2 = pg.transform.flip(bg_img, True, False)
-    kk_img = pg.image.load("fig/3.png")  # こうかとん画像Surafaceを作成する
+    kk_img = pg.image.load("fig/3.png")  
     kk_img = pg.transform.flip(kk_img, True, False)  # こうかとんを左右反転させる
     kk_rct = kk_img.get_rect()  # こうかとんRectを取得する
     kk_rct.center = 300, 200
     tmr = 0
+
     while True:
         for event in pg.event.get():
             if event.type == pg.QUIT: return
         key_lst = pg.key.get_pressed()
-        # print(key_lst[pg.K_UP], key_lst[pg.K_DOWN], 
-        #       key_lst[pg.K_LEFT], key_lst[pg.K_RIGHT])
-        if key_lst[pg.K_UP]:  # 上矢印キーが押されたら
+
+        if key_lst[pg.K_UP]:  
             kk_rct.move_ip(0, -1)
-        if key_lst[pg.K_DOWN]:  # 下矢印キーが押されたら
+        if key_lst[pg.K_DOWN]:  
             kk_rct.move_ip(0, +1)
-        if key_lst[pg.K_LEFT]:  # 左矢印キーが押されたら
+        if key_lst[pg.K_LEFT]:  
             kk_rct.move_ip(-1, 0)
-        if key_lst[pg.K_RIGHT]:  # 右矢印キーが押されたら
+        if key_lst[pg.K_RIGHT]:  
             kk_rct.move_ip(+1, 0)
 
         x = -(tmr%3200)
